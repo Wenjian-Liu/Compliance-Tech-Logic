b95e08da91f19e57bad45f58de518e5f09e53a98
Wenjian-Liu <artshirly@126.com>
2025-12-26 23:03:12 +0800

    feat: override hashCode consistent with equals (Day29)

--- code/oop/HashCodeDemo.java
+++ code/oop/HashCodeDemo.java
@@ -1,6 +1,6 @@
 package com.itheima.chap01;
 
-public class HashcodeDemo {
+public class HashCodeDemo {
     public static void main(String args[]) {
         Person p1 = new Person("小明",18);
         Person p2 = new Person("小明",18);
@@ -9,6 +9,7 @@ public class HashcodeDemo {
         System.out.println(p1.hashCode());
         System.out.println(p1.hashCode());
         System.out.println("--------------------------");
+        System.out.println(p2.hashCode());
         System.out.println(p2.hashCode());
     }
 }
